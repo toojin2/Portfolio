a5828e0f6c6a5983290666b3d8df6c181d9691a7
wlsdl1025 <147586183+wlsdl1025@users.noreply.github.com>
2024-03-31 14:13:39 +0900

    1

---
+++ src/main.js
@@ -0,0 +1,129 @@
+"use strict";
+//header toggle버튼
+document.addEventListener("DOMContentLoaded", function () {
+  const headerBtn = document.querySelector(".header_btn");
+  const gnbToggle = document.getElementById("gnbToggle");
+  const gnb = document.querySelector(".gnb");
+  const gnbBack = document.querySelector(".gnb_backDrop");
+  const gnbLinks = document.querySelectorAll(".gnb_box_tit a");
+
+  headerBtn.addEventListener("click", function () {
+    if (gnbToggle.classList.contains("close")) {
+      gnbToggle.classList.remove("close");
+      gnb.classList.remove("open");
+    } else {
+      gnbToggle.classList.add("close");
+      gnb.classList.add("open");
+    }
+  });
+  // .gnb_backDrop 클래스 클릭 시 메뉴 닫기
+  gnbBack.addEventListener("click", function () {
+    gnb.classList.remove("open");
+    gnbToggle.classList.remove("close");
+  });
+
+  //gnb a태그 클릭시 이동
+  gnbLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      // 링크의 기본 동작을 중단
+      event.preventDefault();
+
+      // 링크의 href 속성 값으로 이동
+      const targetId = this.getAttribute("href");
+      document.querySelector(targetId).scrollIntoView({
+        behavior: "smooth",
+      });
+
+      gnb.classList.remove("open");
+      gnbToggle.classList.remove("close");
+    });
+  });
+
+  const topBox = document.querySelector(".personality_box_top");
+  const bottomBox = document.querySelector(".personality_box_bottom");
+
+  const topTexts = topBox.innerHTML.trim(); // personality top 공백 제거
+  const bottomTexts = bottomBox.innerHTML.trim(); // personality bottom 공백 제거
+
+  topBox.innerHTML += topTexts;
+  bottomBox.innerHTML += bottomTexts;
+
+  // .project_box 요소 선택
+  const projectBoxes = document.querySelectorAll(".project_box");
+
+  // 터치
+  projectBoxes.forEach(function (box) {
+    box.addEventListener("touchstart", function () {
+      this.classList.add("active");
+    });
+  });
+
+  // 마우스 이벤트
+  projectBoxes.forEach(function (box) {
+    box.addEventListener("mouseenter", function () {
+      this.classList.add("active");
+    });
+  });
+  projectBoxes.forEach(function (box) {
+    box.addEventListener("mouseleave", function () {
+      this.classList.remove("active");
+    });
+  });
+});
+//year, time 가져오기
+document.addEventListener("DOMContentLoaded", function () {
+  function getClock() {
+    let today = new Date();
+    let hours = String(today.getHours()).padStart(2, "0");
+    let minutes = String(today.getMinutes()).padStart(2, "0");
+    let seconds = String(today.getSeconds()).padStart(2, "0");
+    const timeString = hours + ":" + minutes + ":" + seconds;
+    document.querySelector(".time").innerText = timeString;
+  }
+
+  let year = new Date().getFullYear(); //year
+  document.querySelector(".contact_bottom .year").innerText = "©" + year;
+  document.querySelector(".order-menu .year").innerText = "©" + year;
+
+  getClock(); // 맨 처음에 한 번 실행
+  setInterval(getClock, 1000); // 1초 주기로 새로 실행
+});
+
+//마우스 커서
+const mouse = document.querySelector(".mouse-cursor");
+const cursor = document.querySelector(".cursor");
+const cursor2 = document.querySelector(".text");
+
+//커서 좌표값 할당
+window.addEventListener("mousemove", (e) => {
+  //GSAP
+  gsap.to(cursor, { duration: 0.5, left: e.pageX - 5, top: e.pageY - 5 });
+  gsap.to(cursor2, { duration: 0.8, left: e.pageX + 25, top: e.pageY - 10 });
+
+  document.querySelectorAll(".img_box").forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      cursor2.classList.add("active");
+    });
+    el.addEventListener("mouseleave", () => {
+      cursor2.classList.remove("active");
+    });
+  });
+
+  document.querySelectorAll(".contact_list").forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      cursor.classList.add("active");
+    });
+    el.addEventListener("mouseleave", () => {
+      cursor.classList.remove("active");
+    });
+  });
+
+  document.querySelectorAll(".profile_box").forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      cursor.classList.add("active2");
+    });
+    el.addEventListener("mouseleave", () => {
+      cursor.classList.remove("active2");
+    });
+  });
+});
---
+++ src/project.js
@@ -0,0 +1,325 @@
+"use strict";
+// gsap
+document.querySelectorAll(".split, .index").forEach((desc) => {
+  let splitText = desc.innerText;
+  let splitWrap = splitText.split("").join("</span><span aria-hidden:'true'>");
+  splitWrap = "<span aria-hidden:'true'>" + splitWrap + "</span>";
+  desc.innerHTML = splitWrap;
+  desc.setAttribute("aria-label", splitText);
+}); //.preloader_logo, index span 만들기
+
+//초기 세팅
+gsap.set(".preloader_logo span", {
+  opacity: 0,
+  scale: 0,
+  display: "inline-block",
+}); //load text
+gsap.set(
+  ".preloader_logo span:nth-child(4), .preloader_logo span:nth-child(8)",
+  {
+    marginRight: "0.5rem",
+  }
+);
+gsap.set(".preloader", {
+  height: "100%",
+}); //preloader
+gsap.set(".home_top_inr .home_inr_img, .home_bottom_inr .home_inr_img", {
+  opacity: 0,
+  scale: 0.1,
+}); //home port, home folio
+gsap.set(".home_middle_inr", { opacity: 0, x: "-100%" }); //home middle
+gsap.set(".home_middle_inr h2, .home_middle_inr p", {
+  opacity: 0,
+  y: "-100%",
+});
+
+// gsap 메인 실행
+setTimeout(() => {
+  let tl = gsap.timeline();
+  tl.to(".preloader_logo span", {
+    opacity: 1,
+    scale: 1,
+    duration: 0.5,
+    stagger: 0.1,
+    ease: Power3.easeInOut,
+  });
+  tl.to(".reversal", {
+    scaleX: -1,
+    duration: 0.3,
+    ease: Power3.easeInOut,
+  });
+  // 반응형 preloader_inr 애니메이션
+  ScrollTrigger.matchMedia({
+    "(min-width: 1381px)": function () {
+      tl.to(".preloader_inr", {
+        top: "24px",
+        left: "64px",
+        translateX: "64px",
+        translateY: "24px",
+        duration: 0.5,
+      });
+    },
+    "(max-width: 1380px)": function () {
+      tl.to(".preloader_inr", {
+        top: "24px",
+        left: "32px",
+        transform: "translate(0px,0px)",
+        duration: 0.5,
+      });
+    },
+    "(max-width: 768px)": function () {
+      tl.to(".preloader_inr", {
+        top: "24px",
+        left: "12px",
+        transform: "translate(8px,0px)",
+        duration: 0.5,
+      });
+    },
+    all: function () {
+      // 다른 화면 크기에 대한 처리
+    },
+  });
+  tl.to(".preloader", {
+    height: 0,
+    duration: 0.2,
+    ease: Power3.easeInOut,
+  });
+  tl.to(".preloader_inr", {
+    opacity: 0,
+    duration: 0.3,
+  }); //preloder 숨기기
+  tl.to(
+    ".home_top_inr .home_inr_img",
+    {
+      opacity: 1,
+      scale: 1,
+      duration: 0.5,
+      stagger: 0.1,
+      ease: Power3.ease,
+    },
+    "ee +=0.5"
+  );
+  tl.to(
+    ".home_bottom_inr .home_inr_img",
+    { opacity: 1, scale: 1, duration: 0.5, stagger: 0.1, ease: Power3.ease },
+    "ee +=1"
+  );
+  tl.to(
+    ".home_middle_inr",
+    { opacity: 1, x: "0%", duration: 0.5, ease: Circ.easOut },
+    "ee +=1.5"
+  );
+  tl.to(".home_middle_inr h2", { opacity: 1, y: "0%", duration: 1 }, "ee +=2");
+  tl.to(".home_middle_inr p", { opacity: 1, y: "0%", duration: 1 }, "ee +=2.5");
+}, 300);
+
+//ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".project_inr_box1", {
+  scrollTrigger: {
+    trigger: ".project1",
+    start: "-100% top",
+    end: "-100% bottom",
+    scrub: 1,
+    //markers: true,
+  },
+  scaleX: 1.0,
+  scaleY: 1.0,
+  scaleZ: 1.3,
+  duration: 0.1,
+  ease: "power1.inOut",
+  startAt: { scale: 0.9 }, // 초기 너비 설정
+}); //project_inr_box
+
+gsap.to(".project_inr_box2", {
+  scrollTrigger: {
+    trigger: ".project2",
+    start: "-100% top",
+    end: "-100% bottom",
+    scrub: 1,
+    //markers: true,
+  },
+  scaleX: 1.0,
+  scaleY: 1.0,
+  scaleZ: 1.3,
+  duration: 0.1,
+  ease: "power1.inOut",
+  startAt: { scale: 0.9 }, // 초기 너비 설정
+});
+gsap.to(".project_inr_box3", {
+  scrollTrigger: {
+    trigger: ".project3",
+    start: "-100% top",
+    end: "-100% bottom",
+    scrub: 1,
+    //markers: true,
+  },
+  scaleX: 1.0,
+  scaleY: 1.0,
+  scaleZ: 1.3,
+  duration: 0.1,
+  ease: "power1.inOut",
+  startAt: { scale: 0.9 }, // 초기 너비 설정
+});
+//project_inr_box
+
+gsap.to(".star-deco", {
+  scrollTrigger: {
+    trigger: "#about",
+    start: "-15% 0%",
+    end: "0% 100%",
+    scrub: 2,
+    //markers: true,
+  },
+  opacity: "1",
+  top: "16%",
+  left: "0%",
+  ease: "power1.inOut",
+}); //star-deco
+
+gsap.to(".deco", {
+  rotation: "+=1080",
+  ease: "power1.inOut",
+  scrollTrigger: {
+    trigger: "#about",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 1,
+    //markers: true,
+  },
+}); //deco
+
+gsap.to(".profile_box", {
+  scrollTrigger: {
+    trigger: "#about",
+    start: "8% 0%",
+    end: "0% 100%",
+    scrub: 2,
+    //markers: true,
+  },
+  opacity: 1,
+  y: 0,
+  duration: 1,
+}); //프로필 사진
+
+gsap.to(".profile_img", {
+  scrollTrigger: {
+    trigger: "#about",
+    start: "top 0%",
+    end: "0% 100%",
+    scrub: 2,
+    //markers: true,
+  },
+  opacity: 1,
+  y: 0,
+  duration: 1,
+}); //프로필 text
+
+gsap.to(".contact_img_txt", {
+  scrollTrigger: {
+    trigger: "#contact",
+    start: "0% center",
+    end: "bottom bottom",
+    scrub: 2,
+    //markers: true,
+  },
+  scale: 1,
+  duration: 1.3,
+}); //thank you
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".index1 span",
+    start: "-50% 50%",
+    end: "100% 100%",
+    scrub: 3,
+    pin: true,
+    //markers: true,
+  },
+});
+tl.to(".index1 span", {
+  y: -50,
+  stagger: 0.2,
+  duration: 1,
+});
+tl.to(".index1 span", {
+  y: 0,
+  stagger: 0.2,
+  duration: 1,
+});
+tl.to(".index1 span", {
+  rotateZ: 1080,
+  stagger: 0.2,
+  duration: 1,
+});
+tl.to(".index1 span", {
+  color: "rgba(242, 242, 242,1)",
+  stagger: 0.2,
+  duration: 1,
+});
+//project index
+
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".index2 span",
+    start: "top 50%",
+    end: "300px 100%",
+    scrub: 3,
+    pin: true,
+    //markers: true,
+  },
+});
+tl2.to(".index2 span", {
+  y: -50,
+  stagger: 0.2,
+  duration: 0.5,
+});
+tl2.to(".index2 span", {
+  y: 0,
+  stagger: 0.2,
+  duration: 1,
+});
+tl2.to(".index2 span", {
+  rotateZ: 1080,
+  stagger: 0.2,
+  duration: 1,
+});
+tl2.to(".index2 span", {
+  color: "rgba(242, 242, 242, 1)",
+  stagger: 0.2,
+  duration: 1,
+}); //about index
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".index3 span",
+    start: "top 50%",
+    end: "100% 100%",
+    scrub: 3,
+    pin: true,
+    //markers: true,
+  },
+});
+tl3.to(".index3 span", {
+  y: -50,
+  stagger: 0.2,
+  duration: 0.5,
+});
+tl3.to(".index3 span", {
+  y: 0,
+  stagger: 0.2,
+  duration: 1,
+  delay: 0.5,
+});
+tl3.to(".index3 span", {
+  rotateZ: 1080,
+  stagger: 0.2,
+  duration: 1,
+  delay: 1.5,
+});
+tl3.to(".index3 span", {
+  color: "rgba(242, 242, 242,1)",
+  stagger: 0.2,
+  duration: 1,
+}); //contact index
